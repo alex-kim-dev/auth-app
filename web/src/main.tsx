407b9572cbaacb86882c837e35619c44f67c178a
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2024-06-17 12:54:12 +0500

    feat(web): add base styling

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,8 @@
+import '~/styles/reset.css';
+import '@fontsource/m-plus-1p/400.css';
+import '@fontsource/m-plus-1p/500.css';
+import '~/styles/main.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
