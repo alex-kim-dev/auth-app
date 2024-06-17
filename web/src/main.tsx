e47e3c0c4da9d244ea41b8484b4054fcb6afffac
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2024-06-17 13:33:33 +0500

    feat(web): add error page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,7 +7,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { MainLayout } from '~/views';
+import { MainLayout, ErrorPage } from '~/views';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
@@ -17,6 +17,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />} path='/' />
+        <Route element={<ErrorPage />} path='*' />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
---
+++ web/src/views/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import { useLayoutEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+export const ErrorPage: React.FC = () => {
+  useLayoutEffect(() => {
+    document.title = 'Auth app | Not found';
+  });
+
+  return (
+    <main className='main error text-center'>
+      <h1 className='error__heading'>Oops!</h1>
+      <p className='error__text'>Sorry, an unexpected error has occurred:</p>
+      <p className='error__code'>404 Not Found</p>
+      <p className='error__text'>
+        <Link to='/'>Go to the main page</Link>
+      </p>
+    </main>
+  );
+};
