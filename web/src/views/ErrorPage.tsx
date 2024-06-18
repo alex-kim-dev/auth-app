903d215bd5a8555b8dc78f0dcdacc15740883482
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2024-06-18 22:12:46 +0500

    fix(web): error page spacing

--- web/src/views/ErrorPage.tsx
+++ web/src/views/ErrorPage.tsx
@@ -7,7 +7,7 @@ export const ErrorPage: React.FC = () => {
   });
 
   return (
-    <main className='main error text-center'>
+    <main className='error text-center'>
       <h1 className='error__heading'>Oops!</h1>
       <p className='error__text'>Sorry, an unexpected error has occurred:</p>
       <p className='error__code'>404 Not Found</p>
